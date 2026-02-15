95375a85b998943bca7e4056fb1eb570284e210f
Subaharinii <subahariniguna@gmail.com>
2026-02-15 23:04:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MovieList from "./MovieList";
 import MovieDetail from "./MovieDetail";
+import "./App.css";
 
 function App() {
   return (
